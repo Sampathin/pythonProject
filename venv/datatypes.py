4dcbb74887bed5b0d93696e35b9b942f15de53fd
samspace <sampathinca@gmail.com>
2023-08-11 00:07:36 +0530

    unwanted space removed from datatypes.py

--- venv/datatypes.py
+++ venv/datatypes.py
@@ -21,15 +21,3 @@ print(x.dtype)
 print("Datatype",newy.dtype ,"\n",newy)
 print("Before Datatype change",z.dtype ,"\n", z,"\nAfter Datatype change", newz.dtype, "\n",newz)
 print(newz1)
-
-
-
-
-
-
-
-
-
-
-
-
